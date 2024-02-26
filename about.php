f4f1e787115db3c766ff4e6dc92eff6e6aa6772c
Villarino-christ <155323344+Villarino-christ@users.noreply.github.com>
2024-02-26 11:30:51 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+</head>
+<body>
+    <br><br><br><br><br><br><br><br>
+    <h2 align="center">Welcome to <b>List of Student</b>, your go to resource for accessing a <br>comprehensive list of students from various educational institutions. <br>
+Our website aims to provide a centralized platform where users can easily <br>search and find information about students, researchers,<br> and other individuals seeking student-related data. </h2>
+    
+</body>
+</html>
